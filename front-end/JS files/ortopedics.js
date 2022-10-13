fc7e564dd172059b9a2e424c72446ab0ba1ae4a0
LeonardoNietoR <leonardo.nieto.r@gmail.com>
2022-10-13 15:46:36 -0500

    update table ortopedic

--- front-end/JS files/ortopedics.js
+++ front-end/JS files/ortopedics.js
@@ -1,24 +1,29 @@
 // Variables
 
-const tbodyTablaOrthesis = document.getElementById("table_orthesis-tbody");
 const btnCreateOrthesis = document.getElementById("btnCreateOrthesis");
 const inputOrthesisId = document.getElementById("orthesis_id");
 const inputOrthesisName = document.getElementById("orthesis_name");
 const inputOrthesisBrand = document.getElementById("orthesis_brand");
 const inputOrthesisYear = document.getElementById("orthesis_year");
 const inputOrthesisCategory = document.getElementById("orthesis_categoryId");
+const ulOrtopedicsList = document.getElementById("ortopedic_list");
+const tbodyTablaDetailsOrt = document.getElementById("table_orthesis-tbody");
+const btnEditOrtopedic = document.getElementById("btnEditOrtopedic");
+const btnDeleteOrtopedic = document.getElementById("btnDeleteOrtopedic");
+const btnCloseDetails = document.getElementById("btnCancelOrtopedic");
 const inputOrthesisDescription = document.getElementById(
    "orthesis_description"
 );
+const containerDetailsOrtopedic = document.querySelector(
+   ".container_details-ortopedic"
+);
 
-const ulOrtopedicsList = document.getElementById("ortopedic_list");
-
-// const urlOrthesisRequest =
-//    "https://g400927313eea0e-g7e55587xh9qd4mr.adb.us-ashburn-1.oraclecloudapps.com/ords/admin/orthesis/orthesis";
+// // const urlOrthesisRequest =
+// //    "https://g400927313eea0e-g7e55587xh9qd4mr.adb.us-ashburn-1.oraclecloudapps.com/ords/admin/orthesis/orthesis";
 
 const urlOrthesisRequest = "http://localhost:8080/api";
 
-// CRUD Orthesis ------------------------------------------------------
+// CRUD Ortopedic ------------------------------------------------------
 
 async function readOrthesis() {
    try {
@@ -26,39 +31,47 @@ async function readOrthesis() {
       const data = await response.json();
       console.log(data);
 
+      sessionStorage.setItem("dataOrt", JSON.stringify(data));
+
       llenarListaOrtopedics(data);
-      llenarTablaOrthesis(data);
    } catch (err) {
       console.log(err);
    }
 }
 
 async function createOrthesis() {
-   const dataToSend = {
-      name: inputOrthesisName.value,
-      brand: inputOrthesisBrand.value,
-      year: parseInt(inputOrthesisYear.value),
-      description: inputOrthesisDescription.value,
-      // category_id: parseInt(inputOrthesisCategory.value),
-      // user must create at least one category first, before creating an ortopedic. User must select the category from a dropdown menu with the name, and here we get the id of that name, which is send as {id:#}
-      category: { id: 1 },
-   };
+   if (
+      inputOrthesisName.value !== "" &&
+      inputOrthesisBrand.value !== "" &&
+      inputOrthesisYear.value !== "" &&
+      inputOrthesisDescription.value !== ""
+   ) {
+      const dataToSend = {
+         name: inputOrthesisName.value,
+         brand: inputOrthesisBrand.value,
+         year: parseInt(inputOrthesisYear.value),
+         description: inputOrthesisDescription.value,
+         // category_id: parseInt(inputOrthesisCategory.value),
+         // user must create at least one category first, before creating an ortopedic. User must select the category from a dropdown menu with the name, and here we get the id of that name which is sent as {id:#}
+         category: { id: 1 },
+      };
 
-   try {
-      console.log(dataToSend);
-      const response = await fetch(`${urlOrthesisRequest}/Ortopedic/save`, {
-         method: "POST",
-         body: JSON.stringify(dataToSend),
-         headers: { "Content-type": "application/json" },
-      });
+      try {
+         console.log(dataToSend);
+         const response = await fetch(`${urlOrthesisRequest}/Ortopedic/save`, {
+            method: "POST",
+            body: JSON.stringify(dataToSend),
+            headers: { "Content-type": "application/json" },
+         });
 
-      if (!response.ok) {
-         throw new Error("Error with post");
+         if (!response.ok) {
+            throw new Error("Error with post");
+         }
+
+         location.reload();
+      } catch (err) {
+         console.log(err);
       }
-
-      location.reload();
-   } catch (err) {
-      console.log(err);
    }
 }
 
@@ -110,30 +123,30 @@ readOrthesis();
 const llenarListaOrtopedics = (data) => {
    if (data) {
       data.forEach((item) => {
-         const newListElement = `<li><a href="#">${item.name}</a></li>`;
+         const newListElement = `<li><a href="#" data-id=${item.id} >${item.name}</a></li>`;
          ulOrtopedicsList.insertAdjacentHTML("beforeend", newListElement);
       });
    }
 };
 
-const llenarTablaOrthesis = (data) => {
-   if (data) {
-      data.forEach((item) => {
-         const newOrthesis = tbodyTablaOrthesis.insertRow();
+const llenarTablaDetailsOrtopedic = (data) => {
+   console.log(data);
 
-         newOrthesis.insertCell([0]).innerHTML = item.id;
-         newOrthesis.insertCell([1]).innerHTML = item.name;
-         newOrthesis.insertCell([2]).innerHTML = item.brand;
-         newOrthesis.insertCell([3]).innerHTML = item.year;
-         newOrthesis.insertCell([4]).innerHTML = item.description;
-         newOrthesis.insertCell([5]).innerHTML = item.category_id;
-         newOrthesis.insertCell([
-            6,
-         ]).innerHTML = `<button onClick="habilitarEdicionTabla(this)">Edit</button>`;
-         newOrthesis.insertCell([
-            7,
-         ]).innerHTML = `<button onClick="deleteOrthesis(${item.id})">Delete</button> `;
-      });
+   if (data) {
+      const detailsOrtopedic = tbodyTablaDetailsOrt.insertRow();
+
+      detailsOrtopedic.insertCell([0]).innerHTML = data.id;
+      detailsOrtopedic.insertCell([1]).innerHTML = data.name;
+      detailsOrtopedic.insertCell([2]).innerHTML = data.brand;
+      detailsOrtopedic.insertCell([3]).innerHTML = data.year;
+      detailsOrtopedic.insertCell([4]).innerHTML = data.description;
+      detailsOrtopedic.insertCell([5]).innerHTML = data.category_id;
+      // detailsOrtopedic.insertCell([
+      //    6,
+      // ]).innerHTML = `<button onClick="habilitarEdicionTabla(this)">Edit</button>`;
+      // newOrthesis.insertCell([
+      //    7,
+      // ]).innerHTML = `<button onClick="deleteOrthesis(${item.id})">Delete</button> `;
    }
 };
 
@@ -169,4 +182,17 @@ const crearBtnGuardar = (contenedorBtn) => {
 btnCreateOrthesis.addEventListener("click", (event) => {
    event.preventDefault();
    createOrthesis();
+});
+
+ulOrtopedicsList.addEventListener("click", (event) => {
+   const dataOrtopedics = JSON.parse(sessionStorage.getItem("dataOrt"));
+   const ortopedicInfo = dataOrtopedics.find(
+      (el) => el.id === parseInt(event.target.dataset.id)
+   );
+   containerDetailsOrtopedic.classList.remove("hide");
+   llenarTablaDetailsOrtopedic(ortopedicInfo);
+});
+
+btnCloseDetails.addEventListener("click", () => {
+   containerDetailsOrtopedic.classList.add("hide");
 });
